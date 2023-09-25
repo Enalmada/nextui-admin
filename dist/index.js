a5a740f2f0f3a3d10474e95ae0c65b9d553d96ed
Adam Lane <enalmada@gmail.com>
2023-09-25 15:25:27 -0700

    fix bun build (#5)
    * use type module
    
    * use module
    
    * use bun

--- dist/index.js
+++ dist/index.js
@@ -80,10 +80,8 @@ var BurguerButton = () => {
 };
 
 // src/navbar/darkmodeswitch.ts
-import Image from "next/image";
 import {useRouter} from "next/navigation";
 import {
-Avatar,
 Dropdown,
 DropdownItem,
 DropdownMenu,
@@ -111,7 +109,7 @@ jsxDEV as jsxDEV3
 } from "react/jsx-dev-runtime";
 var UserDropdown = ({ userDropdownConfig }) => {
   const router = useRouter();
-  const { user, items } = userDropdownConfig;
+  const { user, trigger, items } = userDropdownConfig;
   const dropdownItems = items.map((item) => {
     if (item.isSpecial) {
       return jsxDEV3(DropdownItem, {
@@ -141,53 +139,7 @@ var UserDropdown = ({ userDropdownConfig }) => {
     children: [
       jsxDEV3(NavbarItem, {
         children: jsxDEV3(DropdownTrigger, {
-          children: jsxDEV3(Avatar, {
-            id: "avatar",
-            isBordered: true,
-            className: "transition-transform",
-            color: "secondary",
-            name: user.displayName || user.email || undefined,
-            size: "sm",
-            showFallback: true,
-            fallback: jsxDEV3("span", {
-              className: "inline-block h-[2.375rem] w-[2.375rem] overflow-hidden rounded-full bg-gray-100",
-              children: [
-                user.photoURL && jsxDEV3(Image, {
-                  alt: "",
-                  height: "100",
-                  width: "100",
-                  unoptimized: true,
-                  src: user.photoURL
-                }, undefined, false, undefined, this),
-                !user.photoURL && jsxDEV3("svg", {
-                  className: "h-full w-full text-gray-300",
-                  width: "16",
-                  height: "16",
-                  viewBox: "0 0 16 16",
-                  fill: "none",
-                  xmlns: "http://www.w3.org/2000/svg",
-                  children: [
-                    jsxDEV3("rect", {
-                      x: "0.62854",
-                      y: "0.359985",
-                      width: "15",
-                      height: "15",
-                      rx: "7.5",
-                      fill: "white"
-                    }, undefined, false, undefined, this),
-                    jsxDEV3("path", {
-                      d: "M8.12421 7.20374C9.21151 7.20374 10.093 6.32229 10.093 5.23499C10.093 4.14767 9.21151 3.26624 8.12421 3.26624C7.0369 3.26624 6.15546 4.14767 6.15546 5.23499C6.15546 6.32229 7.0369 7.20374 8.12421 7.20374Z",
-                      fill: "currentColor"
-                    }, undefined, false, undefined, this),
-                    jsxDEV3("path", {
-                      d: "M11.818 10.5975C10.2992 12.6412 7.42106 13.0631 5.37731 11.5537C5.01171 11.2818 4.69296 10.9631 4.42107 10.5975C4.28982 10.4006 4.27107 10.1475 4.37419 9.94123L4.51482 9.65059C4.84296 8.95684 5.53671 8.51624 6.30546 8.51624H9.95231C10.7023 8.51624 11.3867 8.94749 11.7242 9.62249L11.8742 9.93184C11.968 10.1475 11.9586 10.4006 11.818 10.5975Z",
-                      fill: "currentColor"
-                    }, undefined, false, undefined, this)
-                  ]
-                }, undefined, true, undefined, this)
-              ]
-            }, undefined, true, undefined, this)
-          }, undefined, false, undefined, this)
+          children: trigger
         }, undefined, false, undefined, this)
       }, undefined, false, undefined, this),
       jsxDEV3(DropdownMenu, {
